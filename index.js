e6335ae197a20dec87ebae931978db1d63f6edb0
aghmnl <aghmnl@gmail.com>
2025-02-07 19:00:47 -0300

    13. Definiendo las acciones de sockets

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import { server } from "./app.js";
 import { IP_SERVER, PORT } from "./constants.js";
 
+import { io } from "./utils/index.js";
+
 server.listen(PORT, () => {
   console.log("######################");
   console.log("###### API REST ######");
   console.log("######################");
   console.log(`http://${IP_SERVER}:${PORT}/api`);
+
+  io.sockets.on("connection", (socket) => {
+    console.log("NUEVO USUARIO CONECTADO");
+
+    socket.on("disconnect", () => {
+      console.log("USUARIO DESCONECTADO");
+    });
+
+    socket.on("subscribe", (room) => {
+      socket.join(room);
+    });
+
+    socket.on("unsubscribe", (room) => {
+      socket.leave(room);
+    });
+  });
 });
