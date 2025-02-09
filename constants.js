db4fce61c16ac7d71402e7a7e2c8c42269908a33
aghmnl <aghmnl@gmail.com>
2025-02-09 18:58:18 -0300

    14. Conectando una base de datos MongoDB

--- constants.js
+++ constants.js
@@ -1,3 +1,7 @@
+export const DB_USER = "admin";
+export const DB_PASSWORD = "admin123456";
+export const DB_HOST = "chatapp.jbs2i.mongodb.net";
+
 export const IP_SERVER = "localhost";
 
 export const PORT = process.env.PORT || 3977;
