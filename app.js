592454f256231734e70f887b8bc9e54cd9bf899a
aghmnl <aghmnl@gmail.com>
2025-02-09 19:51:49 -0300

    15. Configurando Body Parser

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import http from "http";
 
 import { initSocketServer } from "./utils/index.js";
@@ -7,5 +9,9 @@ const app = express();
 const server = http.createServer(app);
 
 initSocketServer(server);
+
+// Configure Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 export { server };
