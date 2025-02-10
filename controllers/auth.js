7086acf96869a3db6e8122cf76d8dbae2e65068a
aghmnl <aghmnl@gmail.com>
2025-02-10 16:52:27 -0300

    22. Endpoint registro de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,21 @@
 import { User } from "../models/index.js";
 
 function register(req, res) {
-  console.log(req.body);
-  res.status(201).send({ msg: "Todo OK" });
+  const { email, password } = req.body;
+
+  const user = new User({
+    email: email.toLowerCase(),
+    password: password,
+  });
+
+  user.save((error, userStorage) => {
+    if (error) {
+      console.log(error);
+      res.status(400).send({ msg: "Error al registrar el usuario" });
+    } else {
+      res.status(201).send(userStorage);
+    }
+  });
 }
 
 export const AuthController = { register };
