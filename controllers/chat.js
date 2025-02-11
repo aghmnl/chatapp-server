c58339435748c1d905b5b534ff4bb4d4b16019df
aghmnl <aghmnl@gmail.com>
2025-02-11 14:14:43 -0300

    34. Creando un nuevo chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,7 +1,35 @@
-import {} from "../models/index.js";
+import { Chat } from "../models/index.js";
 
 async function create(req, res) {
-  res.status(200).send("Creación OK");
+  const { participant_id_one, participant_id_two } = req.body;
+
+  const foundOne = await Chat.findOne({
+    participant_one: participant_id_one,
+    participant_two: participant_id_two,
+  });
+
+  const foundTwo = await Chat.findOne({
+    participant_one: participant_id_two,
+    participant_two: participant_id_one,
+  });
+
+  if (foundOne || foundTwo) {
+    res.status(200).send({ msg: "Ya tienes un chat con este usuario" });
+    return;
+  }
+
+  const chat = new Chat({
+    participant_one: participant_id_one,
+    participant_two: participant_id_two,
+  });
+
+  chat.save((error, chatStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el chat" });
+    } else {
+      res.status(201).send(chatStorage);
+    }
+  });
 }
 
 export const ChatController = { create };
