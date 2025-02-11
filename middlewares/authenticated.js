fba57f8d354cb0b6b054f9260769e12593195384
aghmnl <aghmnl@gmail.com>
2025-02-11 07:06:39 -0300

    26. Middleware de autenticación - test middleware

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -2,6 +2,7 @@ import { jwt } from "../utils/index.js";
 
 function asureAuth(req, res, next) {
   console.log("middleware ejecutado");
+  next();
 }
 
 export const mdAuth = { asureAuth };
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { AuthController } from "../controllers/index.js";
+import { mdAuth } from "../middlewares/index.js";
 
 const api = express.Router();
 
@@ -7,7 +8,7 @@ api.post("/auth/register", AuthController.register);
 api.post("/auth/login", AuthController.login);
 api.post("/auth/refresh_access_token", AuthController.refreshAccessToken);
 
-api.get("/auth/test_md", (req, res) => {
+api.get("/auth/test_md", [mdAuth.asureAuth], (req, res) => {
   res.status(200).send({ msg: "Todo OK" });
 });
 
