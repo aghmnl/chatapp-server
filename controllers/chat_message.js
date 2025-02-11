ee62d611a7b6d8066756e5c9c06cf602857b9f16
aghmnl <aghmnl@gmail.com>
2025-02-11 16:43:21 -0300

    40. Enviar un mensaje de texto

--- controllers/chat_message.js
+++ controllers/chat_message.js
@@ -1,3 +1,23 @@
 import { ChatMessage } from "../models/index.js";
 
-export const ChatMessageController = {};
+function sendText(req, res) {
+  const { chat_id, message } = req.body;
+  const { user_id } = req.user;
+
+  const chat_message = new ChatMessage({
+    chat: chat_id,
+    user: user_id,
+    message,
+    type: "TEXT",
+  });
+
+  chat_message.save(async (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al enviar el mensaje" });
+    } else {
+      res.status(201).send({});
+    }
+  });
+}
+
+export const ChatMessageController = { sendText };
