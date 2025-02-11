825d1c02657ace673c4e0b268fc2959d47bcdae0
aghmnl <aghmnl@gmail.com>
2025-02-11 08:56:49 -0300

    28. Obtener datos del usuario logueado

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,19 @@
 import { User } from "../models/index.js";
 
 async function getMe(req, res) {
-  res.status(200).send("OK");
+  const { user_id } = req.user;
 
-  // const { user_id } = req.user;
+  try {
+    const response = await User.findById(user_id).select(["-password"]);
 
-  // try {
-  //   const response = await User.findById(user_id).select(["-password"]);
-
-  //   if (!response) {
-  //     res.status(400).send({ msg: "No se ha encontrado el usuario" });
-  //   } else {
-  //     res.status(200).send(response);
-  //   }
-  // } catch (error) {
-  //   res.status(500).send({ msg: "Error del servidor" });
-  // }
+    if (!response) {
+      res.status(400).send({ msg: "No se ha encontrado el usuario" });
+    } else {
+      res.status(200).send(response);
+    }
+  } catch (error) {
+    res.status(500).send({ msg: "Error del servidor" });
+  }
 }
 
 export const UserController = { getMe };
