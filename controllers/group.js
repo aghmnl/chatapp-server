bc50203e4906cc0853ae4fc2ade15bcee19084f6
aghmnl <aghmnl@gmail.com>
2025-02-12 18:21:54 -0300

    49. Obteniendo todos los grupos de un usuario

--- controllers/group.js
+++ controllers/group.js
@@ -26,4 +26,18 @@ function create(req, res) {
   });
 }
 
-export const GroupController = { create };
+function getAll(req, res) {
+  const { user_id } = req.user;
+
+  Group.find({ participants: user_id })
+    .populate("creator", "-password")
+    .populate("participants", "-password")
+    .exec(async (error, groups) => {
+      if (error) {
+        res.status(500).send({ msg: "Error al obtener los grupos" });
+      }
+      res.status(200).send(groups);
+    });
+}
+
+export const GroupController = { create, getAll };
