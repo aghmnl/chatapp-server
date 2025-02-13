a3a50f8adfccede72af14cadae4568544d85e0ae
aghmnl <aghmnl@gmail.com>
2025-02-13 20:05:35 -0300

    58. Enviando mensaje de texto

--- controllers/group_message.js
+++ controllers/group_message.js
@@ -1,4 +1,27 @@
 import { GroupMessage } from "../models/index.js";
 import { io, getFilePath } from "../utils/index.js";
 
-export const GroupMessageController = {};
+function sendText(req, res) {
+  const { group_id, message } = req.body;
+  const { user_id } = req.user;
+
+  const group_message = new GroupMessage({
+    group: group_id,
+    user: user_id,
+    message,
+    type: "TEXT",
+  });
+
+  group_message.save(async (error) => {
+    if (error) {
+      res.status(500).send({ msg: "Error del servidor" });
+    } else {
+      const data = await group_message.populate("user");
+      io.sockets.in(group_id).emit("message", data);
+      io.sockets.in(`${group_id}_notify`).emit("message_notify", data);
+      res.status(201).send({});
+    }
+  });
+}
+
+export const GroupMessageController = { sendText };
--- routes/group_message.js
+++ routes/group_message.js
@@ -7,4 +7,6 @@ const mdUpload = multiparty({ uploadDir: "./uploads/images" });
 
 const api = express.Router();
 
+api.post("/group/message", [mdAuth.asureAuth], GroupMessageController.sendText);
+
 export const groupMessageRoutes = api;
